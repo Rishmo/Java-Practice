f38c17f47c53d12fa117942e0caf7972c94069d3
Moon Sahu <100657272+Rishmo@users.noreply.github.com>
2025-08-19 09:58:17 +0530

    Some pattern problems

---
+++ Pattern_Q16.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Pattern_Q16
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.print("Input number of rows: ");
+int num=sc.nextInt();
+
+for(int i=1;i<=num;i++)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print(" "+i);
+}
+System.out.println(" ");
+}
+}
+}
