c381d4b62ea2c4c7d3730fd0aedc12e838205729
Moon Sahu <100657272+Rishmo@users.noreply.github.com>
2025-08-22 13:00:53 +0530

    applet

---
+++ MyApplet1.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.applet.*;
+public class MyApplet1 extends Applet
+/*
+<applet code=MyApplet.class height=300 width=300>
+</applet>
+*/
+{
+public void paint(Graphics g)
+{
+g.drawString("hello my first applet",10,100);
+}
+}
